f1000cbe65ac8b4b3c0fd1b86087d21a6ec4063e
Sébastien Chopin <seb@nuxt.com>
2023-10-06 11:24:16 +0200

    chore: init

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <UHeader title="Nuxt UI Pro">
+    <template #right>
+      <UButton to="https://ui.nuxt.com/pro" icon="i-heroicons-book-open" target="_blank" color="gray" variant="ghost" />
+      <UColorModeButton />
+    </template>
+  </UHeader>
+  <UContainer>
+    <NuxtPage />
+  </UContainer>
+  <UFooter />
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <UMain>
+    <ULandingHero title="Welcome to Nuxt UI Pro!" description="This is your starter, make something unique!">
+      <template #links>
+        <UButton to="/features" icon="i-heroicons-rocket-launch">Features</UButton>
+        <UButton to="https://ui.nuxt.com/pro" target="_blank" color="gray" icon="i-heroicons-book-open" trailing-icon="i-heroicons-arrow-top-right-on-square-solid">UI Pro Documentation</UButton>
+      </template>
+    </ULandingHero>
+    <ULandingSection>
+      <template #title>
+        Amazing <span class="text-primary">Features</span>
+      </template>
+      <template #description>
+        Welcome to my amazing landing page made with Nuxt UI Pro, look at all these amazing fake features!
+      </template>
+      <UPageGrid>
+        <ULandingCard icon="i-heroicons-banknotes" title="Feature #1" description="This is the number one feature you are dreaming of!" />
+        <ULandingCard icon="i-heroicons-beaker" title="Feature #2" description="I am an important feature, please look at me." />
+        <ULandingCard icon="i-heroicons-bolt" title="Feature #3" description="This is the third feature you are looking at, it looks nice right?" />
+      </UPageGrid>
+    </ULandingSection>
+  </UMain>
+</template>
