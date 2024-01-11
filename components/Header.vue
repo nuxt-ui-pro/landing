e4debfb4fb8d96cfd05038b2382046031a5c8a05
Benjamin Canac <canacb1@gmail.com>
2024-01-11 18:16:31 +0100

    feat(Header): add scrollspy on links
    Resolves #13

--- components/Header.vue
+++ components/Header.vue
@@ -1,25 +1,37 @@
 <script setup lang="ts">
-const links = [{
+const nuxtApp = useNuxtApp()
+const { activeHeadings, updateHeadings } = useScrollspy()
+
+const links = computed(() => [{
   label: 'Features',
   to: '#features',
   icon: 'i-heroicons-cube-transparent',
-  exactHash: true
+  active: activeHeadings.value.includes('features') && !activeHeadings.value.includes('pricing')
 }, {
   label: 'Pricing',
   to: '#pricing',
   icon: 'i-heroicons-credit-card',
-  exactHash: true
+  active: activeHeadings.value.includes('pricing') && !activeHeadings.value.includes('testimonials')
 }, {
   label: 'Testimonials',
   to: '#testimonials',
   icon: 'i-heroicons-academic-cap',
-  exactHash: true
+  active: activeHeadings.value.includes('testimonials')
 }, {
   label: 'FAQ',
   to: '#faq',
   icon: 'i-heroicons-question-mark-circle',
-  exactHash: true
-}]
+  active: activeHeadings.value.includes('faq')
+}])
+
+nuxtApp.hooks.hookOnce('page:finish', () => {
+  updateHeadings([
+    document.querySelector('#features'),
+    document.querySelector('#pricing'),
+    document.querySelector('#testimonials'),
+    document.querySelector('#faq')
+  ])
+})
 </script>
 
 <template>
--- pages/index.vue
+++ pages/index.vue
@@ -37,20 +37,20 @@ defineOgImage({
       </ULandingLogos>
     </ULandingHero>
 
-    <ULandingSection id="features" :title="page.features.title" :description="page.features.description" :headline="page.features.headline">
-      <UPageGrid>
+    <ULandingSection :title="page.features.title" :description="page.features.description" :headline="page.features.headline">
+      <UPageGrid id="features" class="scroll-mt-[calc(var(--header-height)+140px+128px+96px)]">
         <ULandingCard v-for="(item, index) in page.features.items" :key="index" v-bind="item" />
       </UPageGrid>
     </ULandingSection>
 
-    <ULandingSection id="pricing" :title="page.pricing.title" :description="page.pricing.description" :headline="page.pricing.headline">
-      <UPricingGrid compact>
+    <ULandingSection :title="page.pricing.title" :description="page.pricing.description" :headline="page.pricing.headline">
+      <UPricingGrid id="pricing" compact class="scroll-mt-[calc(var(--header-height)+140px+128px+96px)]">
         <UPricingCard v-for="(plan, index) in page.pricing.plans" :key="index" v-bind="plan" />
       </UPricingGrid>
     </ULandingSection>
 
-    <ULandingSection id="testimonials" :headline="page.testimonials.headline" :title="page.testimonials.title" :description="page.testimonials.description">
-      <UPageColumns class="xl:columns-4">
+    <ULandingSection :headline="page.testimonials.headline" :title="page.testimonials.title" :description="page.testimonials.description">
+      <UPageColumns id="testimonials" class="xl:columns-4 scroll-mt-[calc(var(--header-height)+140px+128px+96px)]">
         <div v-for="(testimonial, index) in page.testimonials.items" :key="index" class="break-inside-avoid">
           <ULandingTestimonial v-bind="testimonial" />
         </div>
@@ -61,7 +61,7 @@ defineOgImage({
       <ULandingCTA v-bind="page.cta" :card="false" />
     </ULandingSection>
 
-    <ULandingSection id="faq" :title="page.faq.title" :description="page.faq.description">
+    <ULandingSection id="faq" :title="page.faq.title" :description="page.faq.description" class="scroll-mt-[var(--header-height)]">
       <ULandingFAQ
         multiple
         :items="page.faq.items"
