d9b8c256f1bca46072fbe16818cd375d81cc0611
Benjamin Canac <canacb1@gmail.com>
2024-03-04 18:53:30 +0100

    chore: switch to `@nuxt/fonts`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,22 +4,10 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
     '@nuxt/ui',
-    '@nuxtjs/fontaine',
-    '@nuxtjs/google-fonts'
+    '@nuxt/fonts'
   ],
   ui: {
     icons: ['heroicons', 'simple-icons']
-  },
-  // Fonts
-  fontMetrics: {
-    fonts: ['DM Sans']
-  },
-  googleFonts: {
-    display: 'swap',
-    download: true,
-    families: {
-      'DM+Sans': [400, 500, 600, 700]
-    }
   },
   devtools: {
     enabled: true
