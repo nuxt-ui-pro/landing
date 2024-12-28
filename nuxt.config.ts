583e88308b24b049acfd7092b2afd58d028ed056
Benjamin Canac <canacb1@gmail.com>
2024-12-28 17:29:16 +0100

    fix(nuxt.config): lint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,21 +10,23 @@ export default defineNuxtConfig({
     '@nuxt/ui'
   ],
 
+  devtools: {
+    enabled: true
+  },
+
   routeRules: {
     // Temporary workaround for prerender regression. see https://github.com/nuxt/nuxt/issues/27490
     '/': { prerender: true }
   },
 
-  devtools: {
-    enabled: true
+  future: {
+    compatibilityVersion: 4
   },
+
+  compatibilityDate: '2024-07-11',
 
   typescript: {
     strict: false
-  },
-
-  future: {
-    compatibilityVersion: 4
   },
 
   eslint: {
@@ -34,7 +36,5 @@ export default defineNuxtConfig({
         braceStyle: '1tbs'
       }
     }
-  },
-
-  compatibilityDate: '2024-07-11'
+  }
 })
