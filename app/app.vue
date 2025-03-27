06d4f4337ff50ee872194b5367f334fad40d27a1
Benjamin Canac <canacb1@gmail.com>
2025-03-27 15:52:30 +0100

    fix(app): add `theme-color` meta

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
+const colorMode = useColorMode()
+
+const color = computed(() => colorMode.value === 'dark' ? '#171717' : 'white')
+
 useHead({
   meta: [
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { key: 'theme-color', name: 'theme-color', content: color }
   ],
   link: [
     { rel: 'icon', href: '/favicon.ico' }
